19a4c62ca5482b9893e45fe5e237aeed3ccf6f34
lengedyun <lengedyun@163.com>
2019-05-22 22:07:16 +0800

    init commit

---
+++ src/main/java/com/lengedyun/springbootmybaitis/SpringbootmybaitisApplication.java
@@ -0,0 +1,13 @@
+package com.lengedyun.springbootmybaitis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmybaitisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmybaitisApplication.class, args);
+    }
+
+}
