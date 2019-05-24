90651963e3e8519308e7595e2478f6300fd1102a
lengedyun <lengedyun@163.com>
2019-05-24 20:00:55 +0800

    修改初始化方法

--- src/main/java/com/lengedyun/springbootmybaitis/SpringbootmybaitisApplication.java
+++ src/main/java/com/lengedyun/springbootmybaitis/SpringbootmybaitisApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootmybaitisApplication {
 
     public static void main(String[] args) {
+        System.out.println("main method");
         SpringApplication.run(SpringbootmybaitisApplication.class, args);
     }
 
